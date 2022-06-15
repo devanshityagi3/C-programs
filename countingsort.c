f42d8d1ddb4c3f427f8d6b4c518c296f5b79147b
Devanshi <62430031+devanshityagi3@users.noreply.github.com>
2022-06-15 22:05:21 +0530

    counting sort to sort array

--- countingsort.c
+++ countingsort.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 void main(){
-    int a[5], i,j,max, n,count[]={0},output[5] ;
+    int a[5], i,j,max=0, n,output[5] ;
     printf("Enter array elements:");
     for(i=0;i<5;i++){
        scanf("%d", &a[i]);
@@ -13,17 +13,24 @@ void main(){
        if(a[i]>max)
          max=a[i];
     }
-    for(i=0;i<n;i++){
+  int count[max+1];
+  printf("count array");
+  for(i=0;i<=max;i++){
+       count[i]=0;     //putting 0 at every index of count array
+       printf("%d\n", count[i]);
+    } 
+    for(i=0;i<5;i++){
         count[a[i]]++;
     }
-    for(i=0;i<=max;i++){
-        count[i]=count[i] + count[i-1]; //creating cummulative array by adding the previous and current element. no new array has been made,
-    }                                   // the changes are made in count array itself.
-     for(i=n-1;i>0;i--){
+    for(i=1;i<=max;i++){       //i starts from 1 because changes are being made to count array only, no new array will be created.
+        count[i]=count[i] + count[i-1]; //creating cummulative array
+    }
+     for(i=4;i>=0;i--){
         output[count[a[i]]-1]= a[i];
+       count[a[i]]--;
     }
     printf("Array after sorting");
-    for(i=0;i<n;i++){
+    for(i=0;i<5;i++){
        printf("%d\n", output[i]);
     }   
 }
